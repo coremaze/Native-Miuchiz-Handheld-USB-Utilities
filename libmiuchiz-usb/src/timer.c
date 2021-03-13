cee1e84cc6f433044ac88bbc8b894bb515b46edc
ChrisMiuchiz <chris000005@gmail.com>
2021-03-12 22:47:15 -0500

    Use 64 bit ints when working with time

--- libmiuchiz-usb/src/timer.c
+++ libmiuchiz-usb/src/timer.c
@@ -17,16 +17,16 @@ void miuchiz_utimer_end(struct Utimer* t) {
 }
 
 uint64_t miuchiz_utimer_elapsed(struct Utimer* t) {
-    uint64_t start_utime = t->start_time.tv_sec  * 1000000 /*1000000 usec per sec*/
+    uint64_t start_utime = t->start_time.tv_sec  * 1000000LL /*1000000 usec per sec*/
     #if defined(unix) || defined(__unix__) || defined(__unix)
-                         + t->start_time.tv_nsec   / 1000;    /*1000 nsec per usec*/
+                         + t->start_time.tv_nsec   / 1000LL;    /*1000 nsec per usec*/
     #elif defined(_WIN32)
                          + t->start_time.tv_usec;
     #endif
 
-    uint64_t end_utime   = t->end_time.tv_sec  * 1000000  /*1000000 usec per sec*/
+    uint64_t end_utime   = t->end_time.tv_sec  * 1000000LL  /*1000000 usec per sec*/
     #if defined(unix) || defined(__unix__) || defined(__unix)
-                         + t->end_time.tv_nsec / 1000;    /*1000 nsec per usec*/
+                         + t->end_time.tv_nsec / 1000LL;    /*1000 nsec per usec*/
     #elif defined(_WIN32)
                          + t->end_time.tv_usec;
     #endif
